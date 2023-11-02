8789c7922e693b185b94e40363f79453e7a2926b
gabrsobr <126105265+gabrsobr@users.noreply.github.com>
2023-11-02 15:37:27 +0100

    Add files via upload

---
+++ WEB/WEB/php.php
@@ -0,0 +1,10 @@
+<?php
+$number = 1;
+?>
+
+<?php
+if $number == 1;
+echo "oui"
+else 
+echo "NON"
+?>
---
+++ WEB/WEB/test.php
@@ -0,0 +1,4 @@
+<?php
+    $numberList = [];
+    for($ii = 0; $i<100; $i++);
+    $numberList.array(mt_rand(1, 100);)
